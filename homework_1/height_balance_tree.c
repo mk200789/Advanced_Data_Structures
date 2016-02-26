65a9612e8169b539390654013cfc295fc5c17f83
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-25 22:15:41 -0500

    defined blocksize and implemnting get_node()

--- homework_1/height_balance_tree.c
+++ homework_1/height_balance_tree.c
@@ -6,12 +6,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define BLOCKSIZE 256
+
+//structure of a node
 typedef struct{
 	int key;
 	struct tree_node *left;
 	struct tree_node *right;
 	int height;
 }tree_node;
+
+tree_node *current_block = NULL;
+tree_node *free_list = NULL;
+int size_left;
+
+//returns a node
+tree_node *get_node(){
+	tree_node *tmp;
+	if (free_list != NULL){
+
+	}
+	else{
+
+	}
+	return tmp;
+}
+
 
 int main(){
 	return 0;
