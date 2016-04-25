1bd8b4dcc3bb7e7cea61ac4389988d7ada35924f
Kim Mok <cafe.mui@gmail.com>
2016-04-24 23:49:34 -0400

    included some global vars

--- homework_2/hw2.c
+++ homework_2/hw2.c
@@ -5,15 +5,15 @@
 			gcc -o hw2 hw2.c
 			./hw2
 */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCKSIZE 256
 
 struct rect_list_t{
 	struct rect_list_t *next;
-	int x_min;
-	int x_max;
-	int y_min;
-	int y_max;
+	int x_min; int x_max;
+	int y_min; int y_max;
 };
 
 struct seg_tree_2d_t {
@@ -23,6 +23,10 @@ struct seg_tree_2d_t {
 	struct seg_tree_2d_t *tree;
 	struct rect_list_t *rect_interval;
 };
+
+struct seg_tree_2d_t *current_block = NULL;
+struct seg_tree_2d_t *free_list = NULL;
+int size_left;
 
 int main(){
 	return 0;
