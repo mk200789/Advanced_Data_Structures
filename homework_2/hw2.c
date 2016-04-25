68c7a5412772d4b10005fd3a4fff63c608a2eaf3
Kim Mok <cafe.mui@gmail.com>
2016-04-24 23:53:58 -0400

    implemented get_node()

--- homework_2/hw2.c
+++ homework_2/hw2.c
@@ -10,23 +10,44 @@
 
 #define BLOCKSIZE 256
 
-struct rect_list_t{
-	struct rect_list_t *next;
+typedef struct rect_l_t{
+	struct rect_l_t *next;
 	int x_min; int x_max;
 	int y_min; int y_max;
-};
+}rect_list_t;
 
-struct seg_tree_2d_t {
+typedef struct seg_t_2d_t {
 	int key;
-	struct seg_tree_2d_t *left;
-	struct seg_tree_2d_t *right;
-	struct seg_tree_2d_t *tree;
+	struct seg_t_2d_t *left;
+	struct seg_t_2d_t *right;
+	struct seg_t_2d_t *tree;
 	struct rect_list_t *rect_interval;
-};
+}seg_tree_2d_t;
 
-struct seg_tree_2d_t *current_block = NULL;
-struct seg_tree_2d_t *free_list = NULL;
+seg_tree_2d_t *current_block = NULL;
+seg_tree_2d_t *free_list = NULL;
 int size_left;
+
+seg_tree_2d_t *get_node(){
+	seg_tree_2d_t *tmp;
+	
+	if (free_list != NULL){
+		tmp = free_list;
+		free_list = free_list -> left;
+	}
+	else{
+		if (current_block == NULL || size_left == 0){
+			current_block = (seg_tree_2d_t *) malloc(BLOCKSIZE * sizeof(seg_tree_2d_t));
+			size_left = BLOCKSIZE;
+		}
+		tmp = current_block++;
+		size_left -= 1;
+	}
+	
+	return tmp;
+	
+}
+
 
 int main(){
 	return 0;
