cc8116cf336e4ad847b153bc0eecc9e6637d65d2
Kim Mok <cafe.mui@gmail.com>
2016-05-07 22:14:30 -0400

    implemented left and right rotation

--- homework_3/hw3.c
+++ homework_3/hw3.c
@@ -24,6 +24,65 @@ o_t *current_block = NULL;
 o_t *free_list = NULL;
 int size_left;
 
+//returns a node
+o_t *get_node(){
+	o_t *tmp;
+	if (free_list != NULL){
+		tmp = free_list;
+		free_list = free_list -> left;
+	}
+	else{
+		if (current_block == NULL || size_left == 0){
+			current_block = (o_t *) malloc(BLOCKSIZE * sizeof(o_t));
+			size_left = BLOCKSIZE;
+		}
+		tmp = current_block++;
+		size_left -= 1;
+	}
+	return tmp;
+}
+
+
+void return_node(o_t *node){
+	node->left = free_list;
+	free_list = node;
+}
+
+void right_rotation(o_t *n){
+	o_t *temp_node;
+	int temp_key;
+
+	temp_node = n->right;
+	temp_key = n->key;
+
+	n->right = n->left;
+	n->key = n->left->key;
+
+	n->left = n->right->left;
+
+	n->right->left = n->right->right;
+
+	n->right->right = temp_node;
+	n->right->key = temp_key;
+}
+
+void left_rotation(o_t *n){
+	o_t *temp_node;
+	int temp_key;
+
+	temp_node = n->left;
+	temp_key = n->key;
+
+	n->left = n->right;
+	n->key  = n->right->key;
+
+	n->right = n->left->right;
+
+	n->left->right = n->left->left;
+	
+	n->left->left  = temp_node;
+	n->left->key   = temp_key;
+}
 
 int main(){
 	return 0;
