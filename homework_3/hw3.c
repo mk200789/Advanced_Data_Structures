612f6866937bf529d46d541ece05761e7bb38744
Kim Mok <cafe.mui@gmail.com>
2016-05-07 21:28:38 -0400

    add o_t structure and initialize glob vars

--- homework_3/hw3.c
+++ homework_3/hw3.c
@@ -10,7 +10,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCKSIZE 256
 
+//structure of o_t
+typedef struct tr_n_t{
+	int key;
+	struct tr_n_t *left;
+	struct tr_n_t *right;
+	int height;
+}o_t;
+
+o_t *current_block = NULL;
+o_t *free_list = NULL;
+int size_left;
 
 
 int main(){
