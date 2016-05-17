dc0385231441fd5fdffbdd3905edffea8d8803ce
Kim Mok <cafe.mui@gmail.com>
2016-05-16 23:07:20 -0400

    empty file for homework4

---
+++ homework_4/hw4.c
@@ -0,0 +1,15 @@
+/*
+	Homework 4
+	
+	Compiles and run with command lines
+			gcc -o hw4 hw4.c
+			./hw4
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLOCKSIZE 256
+
+int main(){
+	return 0;
+}
