4b9c7ba659f0ab43b4d8d319998b0379df267b36
Kim Mok <cafe.mui@gmail.com>
2016-05-19 22:04:27 -0400

    added blocksize and get_node()

--- homework_4/hw4.c
+++ homework_4/hw4.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCKSIZE 256
 #define MAXP 46337  /* prime, and 46337*46337 < 2147483647 */
 #define MAX_HASH_STRINGS 250000
 
@@ -41,6 +42,27 @@ typedef struct htp_l_node {
 	struct htp_l_node *next; 
 } htp_l_node_t; 
 
+
+list_node_t *currentblock = NULL;
+list_node_t *free_list = NULL;
+int size_left;
+
+list_node_t *get_node(){
+	list_node_t *tmp;
+	if (free_list != NULL){
+		tmp = free_list;
+		free_list = free_list->next;
+	}
+	else{
+		if (currentblock == NULL || size_left == 0){
+			currentblock = (list_node_t *)malloc(BLOCKSIZE*sizeof(list_node_t));
+			size_left = BLOCKSIZE;
+		}
+		tmp = currentblock++;
+		size_left -= 1;
+	}
+	return tmp;
+}
 
 
 bf_t *create_bf(){
